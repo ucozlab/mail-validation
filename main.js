cdad83f60ca4dabc174dea8a062cf46fa8ff974f
ucozlab <artem@ucoz-team.net>
2014-12-20 18:58:23 +0200

    finished

--- main.js
+++ main.js
@@ -1,42 +1,55 @@
-﻿$("#element_form :input").change(function () {
-    return Formdata(this.form);
-
-    function Formdata(data) {
-
+﻿var a,b,c,d = false;
+document.querySelector('input[type=text][name=name]').onchange = function(val){
+    if(this.value.length < 3){
+        alert("Your name is too short!");
+        this.classList.remove('normal');
+    }
+    else{
+        this.classList.add('normal');
+        a = true;
+    }
+};
+document.querySelector('input[type=email][name=email]').onchange = function(val){
+    if(this.value.length < 3){
+        alert("Your Email is too short!");
+        this.classList.remove('normal');
+    }
+    else{
         var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-        var a, b, c, d = false;
-        if (pattern.test($("#element_form input[name=f1]").val())) {
-            $("#element_form input[name=f1]").parent().addClass("galochka");
-            a = true;
-        } else {
-            $("#element_form input[name=f1]").parent().removeClass("galochka");
-        }
-
-        if (data.f3 != null && data.f3.value.length > 2) {
-            $("#element_form input[name=f3]").parent().addClass("galochka");
+        if (pattern.test(this.value)) {
+            this.classList.add('normal');
             b = true;
         } else {
-            $("#element_form input[name=f3]").parent().removeClass("galochka");
+            this.classList.remove('normal');
+            alert("Your email is invalid!");
         }
-
-        if (data.f4 != null && data.f4.value.length > 3) {
-            $("#element_form input[name=f4]").parent().addClass("galochka");
-            c = true;
-        } else {
-            $("#element_form input[name=f4]").parent().removeClass("galochka");
-        }
-
-        if ($('#element_form').find('input[type=radio]').is(':checked')) {
-            $("#element_form input[type=radio]").parent().addClass("galochka");
-            d = true;
-        } else {
-            $("#element_form input[type=radio]").parent().removeClass("galochka");
-        }
-
-        if (a && b && c && d) {
-            $("#element_form .elementsubmit").removeClass("cantpress");
-        } else {
-            $("#element_form .elementsubmit").addClass("cantpress");
-        }
+    }
+};
+document.querySelector('input[type=text][name=subject]').onchange = function(val){
+    if(this.value.length < 3){
+        alert("Your name is too short!");
+        this.classList.remove('normal');
+    }
+    else{
+        this.classList.add('normal');
+        c = true;
+    }
+};
+document.querySelector('textarea').onchange = function(val){
+    if(this.value.length < 3){
+        alert("Your message is too short!");
+        this.classList.remove('normal');
+    }
+    else{
+        this.classList.add('normal');
+        d = true;
+    }
+};
+document.querySelector('input[type=submit]').addEventListener('click',function(e){
+    e.preventDefault();
+    if (a && b && c && d) {
+        alert('thank you, your email was send');
+    } else {
+        alert('please fill the mailform fields correctly');
     }
 });
